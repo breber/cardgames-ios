4c5d4af44a0b836a5bb85f28c907fa801710dd54
Jamie Kujawa <james.kujawa@gmail.com>
2012-11-13 14:08:20 -0600

    Added basic setting for computer difficulty. The button will need to be switched. Added the setting to user defaults.

---
+++ CardGames/SettingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.h
+//  CardGames
+//
+//  Created by Jamie Kujawa on 11/13/12.
+//  Copyright (c) 2012 Brian Reber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *computerDifficultyPicker;
+
+@end
---
+++ CardGames/SettingsViewController.m
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.m
+//  CardGames
+//
+//  Created by Jamie Kujawa on 11/13/12.
+//  Copyright (c) 2012 Brian Reber. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@implementation SettingsViewController
+
+- (void)viewDidUnload {
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:[self.computerDifficultyPicker titleForSegmentAtIndex:self.computerDifficultyPicker.selectedSegmentIndex] forKey:@"computerDifficulty"];
+    
+    /*
+     To get the data back out for this setting use:
+     
+     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+     NSData *myEncodedObject = [defaults objectForKey:@"computerDifficulty"];
+     
+     */
+    
+    [self setComputerDifficultyPicker:nil];
+    [super viewDidUnload];
+    
+}
+@end
